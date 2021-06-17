b6b773fedbff10015aeeda4dad3da84a0b314402
Emily <hphan9@myseneca.ca>
2021-06-17 02:41:36 -0400

    add css

--- public/main.js
+++
@@ -1,4 +0,0 @@
-// Access the document object for our web page, which is in the current window
-var document = window.document;
-//this function will run when the HTML pages finish loading,
-window.onload = function () {};
